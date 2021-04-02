ef2e6aa0352c6e8fc21eba6fd7dc001dc14de171
csolarz-ml <carlos.solar@mercadolibre.cl>
2021-04-01 23:34:07 -0300

    Feature/create todo model (#2)
    * Update .gitignore
    
    * define model from file.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # gqlgen-todos
+
+## GraphQL server in golang
+
+### Steps: 
+
+1.- generate schema from gqlgen-todos/graph/schema.graphqls
+```
+gqlgen generate
+```
+
+2.- generate resolvers: over graph/resolver.go run command
+```
+go:generate ./...
+```
--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,12 @@
 package graph
 
+import "github.com/csolarz-ml/gqlgen-todos/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+//go:generate go run github.com/99designs/gqlgen
+type Resolver struct {
+	todos []*model.Todo
+}
