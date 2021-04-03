d9f10fec4e1a83d53da3850cc2ed624afd38ea70
Carlos S <carlos.solar@mercadolibre.cl>
2021-04-03 02:11:32 -0300

    use local repository

--- repository/todo_repository.go
+++ repository/todo_repository.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/csolarz-ml/gqlgen-todos/graph/model"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -28,6 +29,10 @@ type database struct {
 func New() TodoRepository {
 
 	MONGO_DB := os.Getenv("MONGO_DB")
+
+	if MONGO_DB == "" {
+		MONGO_DB = "mongodb://localhost:27017/"
+	}
 
 	options := options.Client().ApplyURI(MONGO_DB).SetMaxPoolSize(100)
 
@@ -55,7 +60,7 @@ func (db *database) Save(todo *model.Todo) {
 
 func (db *database) Find() []*model.Todo {
 	collection := db.client.Database(DATABASE).Collection(COLLECTION)
-	cursor, err := collection.Find(context.TODO(), nil)
+	cursor, err := collection.Find(context.TODO(), bson.D{})
 
 	if err != nil {
 		log.Fatal(err)
